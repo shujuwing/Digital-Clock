c8c584607c6614bf029a0bf44b4eade2f709f71e
shujuwing <shujoyk4@gmail.com>
2025-02-02 11:12:56 +0600

    version_1

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+let hrs = document.getElementById("hrs");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+
+setInterval(()=>{
+    let currentTime = new Date();
+
+    hrs.innerHTML = currentTime.getHours();
+    min.innerHTML = currentTime.getMinutes();
+    sec.innerHTML = currentTime.getSeconds();
+
+}
+)
+
